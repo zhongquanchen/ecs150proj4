326091f13dc6268b6dce5901acd027ae529c90d6
dvosingh <33209163+devinesh-singh@users.noreply.github.com>
2019-11-18 19:07:34 -0800

    Update fs.c
    deleted entire implementation of fs_mount: incorrectly implemented - no longer valid with new struct

--- libfs/fs.c
+++ libfs/fs.c
@@ -45,19 +45,6 @@ struct
 
 int fs_mount(const char *diskname)
 {
-    char* sign = "ECS150FS";
-    /* check if block in the disk can't open */
-    if (block_disk_open(diskname) == -1)
-	return -1;
-    
-    block_read(0, (void*)&sb);
-
-    if(memcmp((char*)&sb.signature, signature, 8 != 0))
-    {
-	printf("read the wrong disk\n");
-	return -1;	
-    }
-
 }
 
 int fs_umount(void)
